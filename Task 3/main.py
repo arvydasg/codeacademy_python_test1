d5b530ffd6776221d1146b966e12af30e6d9f0f4
arvydasg <azegaspa@gmail.com>
2022-12-07 18:38:12 +0200

    feat: added task3 solution

--- Task 3/main.py
+++ Task 3/main.py
@@ -9,3 +9,16 @@ audi = {
     "year": 2005,
     "color": "white",
 }
+
+list = []
+
+
+def showObjectKeys(object):
+    for values in object.values():
+        # print(values)
+        # print(list)
+        list.append(values)
+    print(list)
+
+
+showObjectKeys(audi)
