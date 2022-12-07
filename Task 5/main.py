4937aa0e90ce823850b1f1afa2722d369ad7d3a1
arvydasg <azegaspa@gmail.com>
2022-12-07 20:54:45 +0200

    feat: added task 5 solution

--- Task 5/main.py
+++ Task 5/main.py
@@ -1,5 +1,19 @@
 # Importuokite reikiamus modulius, kad veiktų žemiau nurodytos funkcijos
 
+# sugaisau su sita uzduotimi ilgaiu nei norejau/tikejausi :D
+from modules.math.division import division as divivsion
+from modules.math.subtraction import substraction
+from modules.math.multiplication import multiplication
+from modules.math.composition import composition as addition
+
+# zemiau importuoju one by simply del code editoriaus warnings
+# from modules.numbers.numbers import *
+
+from modules.numbers.numbers import one
+from modules.numbers.numbers import two
+from modules.numbers.numbers import three
+from modules.numbers.numbers import four
+from modules.numbers.numbers import five
 
 # Kitų failų ir žemiau esančio kodo nekeiskite
 a = addition(one, four)
